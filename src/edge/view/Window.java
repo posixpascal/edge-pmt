8e4d847b64f8a28b0477cb89077e1d4a56ba1875
Alexander <Alexander@10.4.19.225>
2014-10-14 09:03:03 +0200

    New Package

--- src/edge/view/Window.java
+++ src/edge/view/Window.java
@@ -1,9 +1,8 @@
-package edge.models;
+package edge.view;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
