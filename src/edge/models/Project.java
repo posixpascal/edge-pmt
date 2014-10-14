bb526b0c095072411ebddc11f271d2211b6123b8
Alexander <Alexander@10.4.19.225>
2014-10-14 09:41:45 +0200

    Build Bug removed.

--- src/edge/models/Project.java
+++ src/edge/models/Project.java
@@ -1,11 +1,5 @@
 package edge.models;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-
 import org.javalite.activejdbc.Model;
 
 public class Project extends Model {
--- src/edge/view/Window.java
+++ src/edge/view/Window.java
@@ -13,11 +13,10 @@ public class Window extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = (AnchorPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
-			/* BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml")); */
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();			
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
