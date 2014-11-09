6d48f3a86a9192082f6ad92f2823a76c796be1d8
Pascal Raszyk <pr@mediatrooper.de>
2014-11-09 16:55:12 +0100

    added more detail.

--- src/edge/controllers/settings/FTPSettingsController.java
+++ src/edge/controllers/settings/FTPSettingsController.java
@@ -1,9 +1,31 @@
 package edge.controllers.settings;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import edge.controllers.SettingsController;
 
 public class FTPSettingsController extends SettingsController {
+	
+	@FXML
+	private void checkConnection(){}
+	
+	@FXML
+	private void saveConnection(){}
+	
+	@FXML
+	private TextField hostField;
+	
+	@FXML
+	private TextField usernameField;
+	
+	@FXML
+	private TextField passwordField;
+	
+	@FXML
+	private TextField pathField;
+	
+	
+	
 	@FXML
 	private void initialize(){
 		System.out.print("FTP settings");
